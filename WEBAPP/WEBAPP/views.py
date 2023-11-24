bd9dd2b272305b037e7ced047d68cfadab610294
kivakuuga <kivakuuga94@gmail.com>
2023-11-24 15:24:41 +0800

    Update views.py

--- WEBAPP/WEBAPP/views.py
+++ WEBAPP/WEBAPP/views.py
@@ -93,7 +93,8 @@ def run_plot(time_value):
     plt.xlabel('Crosssections')
     plt.ylabel('Flow')
     plt.savefig('fig1.png')
-    np.delete(y)
+    y = np.empty_like(y)
+    idx = np.empty_like(idx)
 
 # def run_plot(time_value):
 #     dss_file = "/WEBAPP/Muncie.dss"
@@ -144,5 +145,6 @@ def run_plot2(section_name):
     plt.xlabel('Times')
     plt.ylabel('Flow')
     plt.savefig('/WEBAPP/fig1.png')
-    np.delete(y)
+    x = np.empty_like(x)
+    y = np.empty_like(y)
 # Create your views here.
