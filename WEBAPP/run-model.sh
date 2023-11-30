06622cf9de3878cde7ad658dac2a518e7edc4f35
kivakuuga <kivakuuga94@gmail.com>
2023-11-30 18:11:07 +0800

    Update run-model.sh

--- WEBAPP/run-model.sh
+++ WEBAPP/run-model.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-# usage ./run.sh /sim/sample-model/ Muncie
+# usage ./run.sh /sim/sample-model/ example
 RAS_LIB_PATH=/ras/libs:/ras/libs/mkl:/ras/libs/rhel_8
 export LD_LIBRARY_PATH=$RAS_LIB_PATH:$LD_LIBRARY_PATH
 echo $LD_LIBRARY_PATH
@@ -9,14 +9,14 @@ RAS_EXE_PATH=/ras/v61
 export PATH=$RAS_EXE_PATH:$PATH
 echo $PATH
 
-#delete old p04 hdf results and copy in fixed up one 
-rm Muncie.p04.hdf
-rm Muncie.p04.tmp.hdf
-cp wrk_source/Muncie.p04.tmp.hdf .
+#delete old p01 hdf results and copy in fixed up one 
+rm example.p01.hdf
+rm example.p01.tmp.hdf
+cp wrk_source/example.p01.tmp.hdf .
 
 # remove old results
-rm Muncie.dss
+rm example.dss
 
-RasUnsteady Muncie.c04 b04
+RasUnsteady example.c01 b01
 
-mv Muncie.p04.tmp.hdf Muncie.p04.hdf
+mv example.p01.tmp.hdf example.p01.hdf
