b6f4ce4ed019aca62567e7c6d421e120cc5a44b9
kivakuuga <kivakuuga94@gmail.com>
2023-12-08 11:32:16 +0800

    Update plot.py

--- pydsstools/plot.py
+++ pydsstools/plot.py
@@ -1,46 +1,65 @@
 from pydsstools.heclib.dss import HecDss
+from pydsstools.heclib.dss.HecDss import Open
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 dss_file = "C:\\Users\\PatZh\\Desktop\\xindian\\example.dss"
-pathname = "/Tamsui xindaian//LOCATION-ELEV//02JAN1900 0250/RESULTS/"
-
-def run_plot2(time_value):
+# pathname = "/Tamsui xindaian//LOCATION-ELEV//02JAN1900 0250/RESULTS/"
+def run_plot2(section_name):
     x = []
     y = []
-    section_index = int(-time_value) 
-    #因為矩陣是反的，所以索引值用負的。
-    #例如:斷面0-77，矩陣[0]的數值就是斷面77
+    section_index = int(section_name)
+    section_index = -section_index
     dss_file = 'C:\\Users\\PatZh\\Desktop\\xindian\\example.dss'
-    pathname_pattern = "/*/*/LOCATION-ELEV/*/*/*/"
+    pathname_pattern0 = "/*/*/LOCATION-ELEV/*/*/*/"
+    pathname_pattern1 = "/*/*/LOCATION-FLOW/*/*/*/"
 
-    
     fid = HecDss.Open(dss_file)
-    path_list = fid.getPathnameList(pathname_pattern, sort=1)
     
-    for i in range(len(path_list)):
-        pd = fid.read_pd(path_list[i])
-        idx = pd.index.tolist()
-        pd.insert(0, pd.index.name, idx)
-        pd.index = range(1, pd.shape[0] + 1)
-        np.array(idx)
-
-        x.append(i)
-        value = np.array(pd.iloc[:, 1].values)
-        y.append(value[section_index])
-        print(value)
-    x1 = x[:-16]
-    y1 = y[:-16]
-
-    
-    plt.plot(x1, y1)
-    plt.title('Flow Data')
-    plt.xlabel('Times')
-    plt.ylabel('Flow')
-    plt.savefig('fig1.png')
+    k = 1
+    while k <= 2:
+        fid = HecDss.Open(dss_file)
+        if k == 1:
+            path_list = fid.getPathnameList(pathname_pattern0, sort=1)
+            TYPE = 'ELEV'
+        else:
+            path_list = fid.getPathnameList(pathname_pattern1, sort=1)
+            TYPE = 'FLOW'
+            
+        for i in range(len(path_list)):
+            pd = fid.read_pd(path_list[i])
+            idx = pd.index.tolist()
+            pd.insert(0, pd.index.name, idx)
+            pd.index = range(1, pd.shape[0] + 1)
+            np.array(idx)
+            x.append(i)
+            value = np.array(pd.iloc[:, 1].values)
+            y.append(value[section_index])
+            #x1及y1的修剪數目可能根據hec-ras版本會有變，需要注意
+            if k == 1:
+                x1 = x[:-5]
+                y1 = y[:-5]
+                plt.subplot(2,1, k)
+                plt.plot(x1,y1,color = 'b',linewidth ='1')
+                plt.title(f'NO.{section_name} {TYPE} Data')
+                plt.xlabel('Times')
+                plt.ylabel('Flow')
+            else:
+                x2 = x[:-5]
+                y2 = y[:-5] 
+                plt.subplot(2,1, k) 
+                plt.plot(x2,y2,color = 'g',linewidth ='1')
+                plt.title(f'NO.{section_name} {TYPE} Data')  
+                plt.xlabel('Times')
+                plt.ylabel('Flow')    
+        x = []
+        y = []
+              
+        k = k + 1
+    plt.tight_layout()
+    plt.savefig('fig.png')
     plt.close()
-    x = np.empty_like(x)
-    y = np.empty_like(y)
-    return y1[0]
-print(run_plot2(27))
+
+
+run_plot2(27)
