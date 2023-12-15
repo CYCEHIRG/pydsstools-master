581fe51833731db2ea2ffb36dc8964c0daf857eb
kivakuuga <kivakuuga94@gmail.com>
2023-12-15 11:07:04 +0800

    Update views.py

--- WEBAPP/WEBAPP/views.py
+++ WEBAPP/WEBAPP/views.py
@@ -11,7 +11,7 @@ from pydsstools.heclib.dss.HecDss import Open
 import numpy as np
 import matplotlib.pyplot as plt
 import h5py
-import sys,io
+import sys,io,os
 from shutil import copyfile
 
 folder_path = '/WEBAPP'
@@ -154,11 +154,15 @@ def run_plot2(section_name):
             if k == 1:
                 x1 = x[:-5]
                 y1 = y[:-5]
+                obs = [3.29,3.31,3.3,3.2,3.06,3.04,3.04,3.07,3.08,3.03,3.04,3.2,3.29,3.24
+                       ,3.22,3.21,3.23,3.25,3.24,3.25,3.27,3.26,3.2,3.01,3.18]
                 plt.subplot(2,1, k)
-                plt.plot(x1,y1,color = 'b',linewidth ='1')
+                line1, = plt.plot(x1,y1,color = 'b',linewidth ='1',label = 'sim')
+                line2, = plt.plot(x1,obs,color = 'r', linewidth = '1',label = 'obs')
+                plt.legend(handles = [line1,line2],loc = 'upper right')
                 plt.title(f'NO.{section_name} {TYPE} Data')
                 plt.xlabel('Times')
-                plt.ylabel('Flow')
+                plt.ylabel('ELEV')
             else:
                 x2 = x[:-5]
                 y2 = y[:-5] 
