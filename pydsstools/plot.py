56f681c12dece16d25c8f0cd6de24047070e1a11
kivakuuga <kivakuuga94@gmail.com>
2023-12-22 10:37:14 +0800

    Update plot.py

--- pydsstools/plot.py
+++ pydsstools/plot.py
@@ -1,22 +1,34 @@
+
+import csv
+import subprocess
+import glob
+import pandas as pd
 from pydsstools.heclib.dss import HecDss
 from pydsstools.heclib.dss.HecDss import Open
-import matplotlib.pyplot as plt
 import numpy as np
-
-
-dss_file = "C:\\Users\\PatZh\\Desktop\\xindian\\example.dss"
-# pathname = "/Tamsui xindaian//LOCATION-ELEV//02JAN1900 0250/RESULTS/"
+import matplotlib.pyplot as plt
+import h5py
+import sys,io,os
+from shutil import copyfile
 def run_plot2(section_name):
+    #因HEC-RAS中沒有指儲存單一斷面結果的路徑，因此另外撰寫
+    #只繪製單一斷面結果的圖，由使用者輸入數字給定
+    #目前限制不得輸入負數，若有需要得以在result.html中修改
     x = []
     y = []
+    #更改字串數字格式
     section_index = int(section_name)
+    #在獲取單一斷面之結果時陣列是反的，所以加上負號由後面開始取
     section_index = -section_index
+    #目前.dss的名稱指定為example
     dss_file = 'C:\\Users\\PatZh\\Desktop\\xindian\\example.dss'
+    #.dss指定的路徑格式，ELEV表示水位，FLOW表示流量
     pathname_pattern0 = "/*/*/LOCATION-ELEV/*/*/*/"
     pathname_pattern1 = "/*/*/LOCATION-FLOW/*/*/*/"
 
     fid = HecDss.Open(dss_file)
     
+    #計數器，用來區分FLOW、ELEVATION的繪圖模式
     k = 1
     while k <= 2:
         fid = HecDss.Open(dss_file)
@@ -36,15 +48,20 @@ def run_plot2(section_name):
             x.append(i)
             value = np.array(pd.iloc[:, 1].values)
             y.append(value[section_index])
-            #x1及y1的修剪數目可能根據hec-ras版本會有變，需要注意
+            #x1、x2及y1、y2的修剪數目可能根據hec-ras版本會有變，需要注意
+            #修剪的目的是，結果檔會有一些模擬時間之外的結果，例如:斷面最大水深等
             if k == 1:
                 x1 = x[:-5]
                 y1 = y[:-5]
+                xobs = np.arange(0,25)
+                obs = [3.29,3.31,3.3,3.2,3.06,3.04,3.04,3.07,3.08,3.03,3.04,3.2,3.29,3.24
+                       ,3.22,3.21,3.23,3.25,3.24,3.25,3.27,3.26,3.2,3.01,3.18]
                 plt.subplot(2,1, k)
                 plt.plot(x1,y1,color = 'b',linewidth ='1')
                 plt.title(f'NO.{section_name} {TYPE} Data')
                 plt.xlabel('Times')
-                plt.ylabel('Flow')
+                plt.ylabel('ELEV')
+
             else:
                 x2 = x[:-5]
                 y2 = y[:-5] 
@@ -54,12 +71,18 @@ def run_plot2(section_name):
                 plt.xlabel('Times')
                 plt.ylabel('Flow')    
         x = []
-        y = []
-              
+        y = []      
         k = k + 1
     plt.tight_layout()
-    plt.savefig('fig.png')
+    plt.savefig('fig1.png')
     plt.close()
+    print(y1)
+    path = 'output_data.csv'
+    with open(path, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for i in range(len(y1)):
+            writer.writerow(y1[i])
 
+# Create your views here.
 
 run_plot2(27)
