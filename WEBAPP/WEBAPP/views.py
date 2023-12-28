8ae7f143686ead86625f00d92fb575acaa79d441
kivakuuga <kivakuuga94@gmail.com>
2023-12-28 12:13:49 +0800

    Update views.py

--- WEBAPP/WEBAPP/views.py
+++ WEBAPP/WEBAPP/views.py
@@ -154,6 +154,9 @@ def run_plot2(section_name):
             if k == 1:
                 x1 = x[:-5]
                 y1 = y[:-5]
+                # xobs = np.arange(0,25)
+                # obs = [3.29,3.31,3.3,3.2,3.06,3.04,3.04,3.07,3.08,3.03,3.04,3.2,3.29,3.24
+                #        ,3.22,3.21,3.23,3.25,3.24,3.25,3.27,3.26,3.2,3.01,3.18]
                 plt.subplot(2,1, k)
                 plt.plot(x1,y1,color = 'b',linewidth ='1',label = 'sim')
                 # line2, = plt.plot(xobs,obs,color = 'r', linewidth = '1',label = 'obs')
